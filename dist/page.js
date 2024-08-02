a397dff090cad264c35ca7a0b4db2f4fe3aaaefd
hamidb80 <hr.bolouri@gmail.com>
2024-08-02 22:34:41 +0330

    wow

--- dist/page.js
+++ dist/page.js
@@ -1,6 +1,8 @@
 
 up.macro('[smooth-link]', link => {
-  link.setAttribute('up-transition', 'cross-fade')
+  // if (!link.hasAttribute('up-transition'))
+  // link.setAttribute('up-transition', 'cross-fade')
+  link.setAttribute('up-transition', 'move-to-bottom/fade-in')
   link.setAttribute('up-duration', '400')
   link.setAttribute('up-follow', '')
 })
