e1ce103223f7587b7a097e23acb5b2c1d72df3af
hamidb80 <hr.bolouri@gmail.com>
2024-08-19 13:41:35 +0330

    1

--- deploy.nim
+++ deploy.nim
@@ -7,20 +7,25 @@ proc exec(cmd: string) =
   if r != 0:
     write stderr, "[STATUS CODE] = " & $r
     quit r
+
+proc cp(src, dest: string) = 
+  case src[^1]
+  of '/', '\\': copyDir  src, dest
+  else        : copyFile src, dest
   
 
 when isMainModule:
   write stdout, "commit message (for main branch): "
   let msg = readLine stdin
 
-  exec      "git add ."
-  exec      "git commit -m \"" & msg & '"'
-  exec      "git push"
-  exec      "nimble dev"
-  copydir   "./dist", "../dist"
-  exec      "git checkout pages"  
-  copydir   "../dist", "./"
-  exec      "git add ."  
-  exec      "git commit -m \".\""  
-  exec      "git push"  
-  exec      "git checkout main"  
+  exec  "git add ."
+  exec  "git commit -m \"" & msg & '"'
+  exec  "git push"
+  exec  "nimble dev"
+  cp    "./dist/", "../dist/"
+  exec  "git checkout pages"  
+  cp    "../dist/", "./"
+  exec  "git add ."  
+  exec  "git commit -m \".\""  
+  exec  "git push"  
+  exec  "git checkout main"  
